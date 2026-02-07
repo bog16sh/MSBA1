845de977d88059026f7e506ccaeeaa90161ebbc7
Bogdan Shalimov <u1611528@umail.utah.edu>
2026-02-06 18:51:27 -0700

    Update author information in data_preparation.R

--- data_preparation.R
+++ data_preparation.R
@@ -3,7 +3,7 @@
 # Modular, reusable functions for consistent data preprocessing
 # Prevents data leakage and ensures train/test consistency
 #
-# Author: Databot
+# Author: Bogdan Shalimov with databot
 # Date: 2026-02-05
 # Purpose: Complete, reproducible preprocessing pipeline
 
